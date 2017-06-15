aab5d5c13c4129915839d4e9324c1988953d87bb
9838317@qq.com <9838317@qq.com>
2017-06-15 09:26:12 +0800

    automatic commit

---
+++ firstxblock/firstxblock.py
@@ -0,0 +1,112 @@
+"""TO-DO: Write a description of what this XBlock is."""
+
+import pkg_resources
+
+from xblock.core import XBlock
+from xblock.fields import Scope, Integer, String
+from xblock.fragment import Fragment
+
+"""
+The aim of this method is to display some images.
+actually we can just store the relative infomations inside the field
+, which will greatly reduce the workload.
+We can store:
+    1. the number of the total pages
+    2. the current page
+"""
+
+class FirstXBlock(XBlock):
+
+
+    baseUrl = String(
+        default = "", scope = Scope.user_state_summary,
+        help = "picture url base"
+    )
+
+    fullUrl = String(
+        default = "", scope = Scope.user_state_summary,
+        help = "picture url url"
+    )
+
+
+    count = Integer(
+        default= 0, scope=Scope.user_state,
+        help="total pages",
+    )
+
+    totalPages = Integer(
+        default= 10, scope=Scope.user_state,
+        help="total pages",
+    )
+    page       = Integer(
+        default= 0, scope=Scope.user_state,
+        help="the current page",
+    )
+
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
+
+    # TO-DO: change this view to display your data your own way.
+    def student_view(self, context=None):
+        """
+        The primary view of the FirstXBlock, shown to students
+        when viewing courses.
+        """
+        html = self.resource_string("static/html/firstxblockStu.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_css(self.resource_string("static/css/firstxblock.css"))
+        frag.add_javascript(self.resource_string("static/js/src/firstxblock.js"))
+        frag.initialize_js('FirstXBlock')
+        return frag
+
+    def studio_view(self, context=None):
+        """
+        The primary view of the paellaXBlock, shown to students
+        when viewing courses.
+        """
+        html = self.resource_string("static/html/firstxblockTea.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_javascript(self.resource_string("static/js/src/pdf_edit.js"))
+        frag.initialize_js('pdfXBlock')
+        return frag
+
+
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    @XBlock.json_handler
+    def get_page(self, data, suffix=''):
+        """
+        An example handler, which increments the data.
+        """
+        newPage = int(data["page"])
+        print newPage
+        print type(data['page'])
+        # Just to show data coming in...
+        if newPage >= 0 and newPage < self.totalPages:
+            self.page = newPage
+
+        self.count += 1
+        #return {"count": self.count, "page":page}
+        print self.page
+        return {"count": self.count, "page": self.page}
+
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("FirstXBlock",
+             """<firstxblock/>
+             """),
+            ("Multiple FirstXBlock",
+             """<vertical_demo>
+                <firstxblock/>
+                <firstxblock/>
+                <firstxblock/>
+                </vertical_demo>
+             """),
+        ]
