43458cb91a6c0311cf9b72f4bce9595cab228f7e
9838317@qq.com <9838317@qq.com>
2017-06-22 09:28:37 +0800

    automatic commit

--- firstxblock/static/js/src/firstxblockStu.js
+++ firstxblock/static/js/src/firstxblockStu.js
@@ -26,7 +26,7 @@ function FirstXBlock(runtime, element) {
     {}
 
     // This will be the basic address that we can send ajax request.
-    var baseUrl = "http://127.0.0.1:8000/filecms/image/";
+    var baseUrl = "http://127.0.0.1:8002/filecms/image/";
 
     // postUrl here is for posting the message to the xblock special handle function.
     var postUrl = runtime.handlerUrl(element, 'get_page');
@@ -121,7 +121,7 @@ function FirstXBlock(runtime, element) {
             //             
             var name       = $('.systemGeneratedRandomName', element).text();
 
-            var baseUrl    = "http://127.0.0.1:8000/filecms/image/";
+            var baseUrl    = "http://127.0.0.1:8002/filecms/image/";
             var getUrl     = baseUrl + "getimagesquantity/";
             var src        = baseUrl + "getimages/" + name + "?page=0";
             var jsonData   = {"imageFolder": name};
